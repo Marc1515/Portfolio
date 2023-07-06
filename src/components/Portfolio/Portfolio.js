27a354999d30551be937e65fd49c0de60dfae0e3
Marc España Quinquilla <meq.1515@gmail.com>
2023-07-06 14:53:33 +0200

    Nuevo proyecto añadido countriesApp.

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -5,52 +5,60 @@ import montsiaClinicAppImg from "./../../assets/img/montsiaClinicApp.png";
 import annaAppImg from "./../../assets/img/annaApp.png";
 import euromafeAppImg from "./../../assets/img/euromafeApp.png";
 import gifsAppImg from "./../../assets/img/gifsApp.png";
+import countriesApp from "./../../assets/img/countriesApp.png";
 
 const PortfolioQuestions = [
   {
     id: 1,
+    nombre: "Countries App",
+    githubURL: "https://github.com/Marc1515/CountriesApp",
+    pageURL: "https://countries.marcespana.com/",
+    img: countriesApp,
+  },
+  {
+    id: 2,
     nombre: "Gifs App",
     githubURL: "https://github.com/Marc1515/gifs-app",
     pageURL: "https://gifs.marcespana.com",
     img: gifsAppImg,
   },
   {
-    id: 2,
+    id: 3,
     nombre: "Learn English App",
     githubURL: "https://github.com/Marc1515/learn-english",
     pageURL: "https://learning.marcespana.com",
     img: learrnAppImg,
   },
   {
-    id: 3,
+    id: 4,
     nombre: "Trailers App",
     githubURL: "https://github.com/Marc1515/movieapp-youtube",
     pageURL: "https://trailers.marcespana.com",
     img: trailersAppImg,
   },
   {
-    id: 4,
+    id: 5,
     nombre: "Weather App",
     githubURL: "https://github.com/Marc1515/openweather",
     pageURL: "https://theweather.marcespana.com",
     img: weatherAppImg,
   },
   {
-    id: 5,
+    id: 6,
     nombre: "Montsia Clinic App",
     githubURL: null,
     pageURL: "https://montsiaclinic.marcespana.com/",
     img: montsiaClinicAppImg,
   },
   {
-    id: 6,
+    id: 7,
     nombre: "Tienda Anna App",
     githubURL: null,
     pageURL: "https://anna.marcespana.com/",
     img: annaAppImg,
   },
   {
-    id: 7,
+    id: 8,
     nombre: "Euromafe App",
     githubURL: null,
     pageURL: "https://euromafe.marcespana.com",
