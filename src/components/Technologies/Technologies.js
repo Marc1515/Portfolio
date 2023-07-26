03cc38b135ea81594660f88b9d3e46302bf72e33
Marc España Quinquilla <meq.1515@gmail.com>
2023-07-26 21:18:27 +0200

    Nuevo componente que muestra un las tecnologías y explica un pequeño fragmento de cada una de ellas para dar a entender cuál es mi conocimiento. En un futuro me gustaría hacer el código un poco más declarativo, ya que siento que aunque no sean muchas líneas aún se puede condensar más.

---
+++ src/components/Technologies/Technologies.js
@@ -0,0 +1,41 @@
+import htmlSvgIMG from "./../../assets/img/svg/html5-color.svg";
+import cssSvgIMG from "./../../assets/img/svg/css3-color.svg";
+import javaScriptSvgIMG from "./../../assets/img/svg/javascript-color.svg";
+import typeScriptSvgIMG from "./../../assets/img/svg/typescript-color.svg";
+import angularSvgIMG from "./../../assets/img/svg/angular-color.svg";
+import rxjsSvgIMG from "./../../assets/img/svg/reactivex-color.svg";
+import reactSvgIMG from "./../../assets/img/svg/react-color.svg";
+import wordpressSvgIMG from "./../../assets/img/svg/wordpress-color.svg";
+import wooCommerceSvgIMG from "./../../assets/img/svg/woo-color.svg";
+import bootstrapSvgIMG from "./../../assets/img/svg/bootstrap-color.svg";
+import tailwindSvgIMG from "./../../assets/img/svg/tailwindcss-color.svg";
+
+
+const TechImages = 
+    {
+        techBuild: {
+            htmlSVG: htmlSvgIMG,
+            cssSVG: cssSvgIMG,
+            javaScriptSvg: javaScriptSvgIMG,
+            angularSVG: angularSvgIMG,
+            rxjsSvg: rxjsSvgIMG,
+            typeScriptSvg: typeScriptSvgIMG,
+            reactSVG: reactSvgIMG,
+            wordpressSVG: wordpressSvgIMG,
+            wooCommerceSVG: wooCommerceSvgIMG,
+            bootstrapSVG: bootstrapSvgIMG,
+            tailwindSvg: tailwindSvgIMG
+        },
+        
+        techDescriptions: {
+            htmlCssJs: "Mi experiencia en HTML me permite estructurar eficazmente el contenido web. A través del CSS, aplico diseños responsivos y atractivos, y con JavaScript, gestiono interacciones dinámicas y manipulación de APIs en mis proyectos web.",
+            tailwindBootstrap: "Utilizo Bootstrap para construir rápidamente interfaces de usuario responsivas con sus componentes predefinidos. Tailwind, en cambio, lo uso cuando necesito un control granular sobre el estilo, ya que proporciona una gran cantidad de clases utilitarias para construir diseños personalizados.",
+            typescriptRxjs: "Como desarrollador entiendo como funciona el superset Typescript, que me permite escribir código más seguro y predecible gracias a su tipado fuerte. También tengo habilidades sólidas con RxJS, una biblioteca de programación reactiva que utilizo para manejar secuencias de eventos asíncronos con los operadores, permitiéndome un flujo de datos más eficiente y escalables.",
+            reactAngular: "Tengo un sólido entendimiento tanto de React como de Angular. Con React, puedo crear interfaces de usuario dinámicas y eficientes, aprovechando su modelo de componentes, la reconciliación de DOM virtual y el uso de los hooks. En Angular, manejo la creación de aplicaciones de página única a gran escala, utilizando su completo marco basado en TypeScript, con una profunda comprensión de los módulos, componentes, servicios, y enrutamiento.",
+            wordpressWoocommerce: "Tengo experiencia en la plataforma de gestión WordPress, para crear sitios web atractivos y funcionales. Adicionalmente, estoy familiarizado con WooCommerce que me permite la creación de tiendas online robustas y personalizables, incluyendo configuración de productos, métodos de pago, y gestión de envíos."
+        }
+        
+    }
+;
+
+export default TechImages;
---
+++ src/components/Technologies/Technologies.jsx
@@ -0,0 +1,171 @@
+import React, { useState } from "react";
+import "./Technologies.css";
+import TechImages from "./Technologies";
+import { FiChevronDown, FiChevronUp } from "react-icons/fi";
+
+const Technologies = () => {
+  const [openSection, setOpenSection] = useState(null);
+
+  const toggleSection = (section) => {
+    setOpenSection((current) => (current === section ? null : section));
+  };
+
+  return (
+    <section id="technologies" className="tech-container">
+
+      {/* Sección -> HTML, CSS y JavaScript */}
+
+      <h1>
+        <span>Tecnologías</span>
+      </h1>
+
+      <span className="title-tech">Básicas</span>
+
+      <div className="container-img-tech">
+        <img src={TechImages.techBuild.htmlSVG} alt="" />
+        <img src={TechImages.techBuild.cssSVG} alt="" />
+        <img src={TechImages.techBuild.javaScriptSvg} alt="" />
+      </div>
+
+      <div
+        className={`custom-details ${openSection === "BT" ? "open" : ""}`}
+        onClick={() => toggleSection("BT")}
+      >
+        <div className="toggle-container">
+          <p className="summary">HTML, CSS y JavaScript</p>
+          <span className="icon-arrow">
+            {openSection === "BT" ? <FiChevronUp /> : <FiChevronDown />}
+          </span>
+        </div>
+        <div className="content">
+          {TechImages.techDescriptions.htmlCssJs}  {/* Descripción de las tecnologías */}
+        </div>
+      </div>
+
+      
+      <hr className="horizontal-line" />
+
+      {/* Sección -> Taildwind y Bootstrap */}
+
+      <span className="title-tech">Librerías para CSS</span>
+
+      <div className="container-img-tech">
+        <img src={TechImages.techBuild.tailwindSvg} alt="" />
+        <img src={TechImages.techBuild.bootstrapSVG} alt="" />
+      </div>
+
+      <div
+        className={`custom-details ${openSection === "LC" ? "open" : ""}`}
+        onClick={() => toggleSection("LC")}
+      >
+        <div className="toggle-container">
+          <p className="summary">Tailwind y Bootstrap</p>
+          <span className="icon-arrow">
+            {openSection === "LC" ? <FiChevronUp /> : <FiChevronDown />}
+          </span>
+        </div>
+        <div className="content">
+          {TechImages.techDescriptions.tailwindBootstrap}  {/* Descripción de las tecnologías */}
+        </div>
+      </div>
+
+      
+      <hr className="horizontal-line" />
+
+      {/* Sección -> Typescript y RxJS */}
+
+      <span className="title-tech">Superset y librerías</span>
+
+      <div className="container-img-tech">
+        <img src={TechImages.techBuild.typeScriptSvg} alt="" />
+        <img src={TechImages.techBuild.rxjsSvg} alt="" />
+      </div>
+
+      <div
+        className={`custom-details ${openSection === "TR" ? "open" : ""}`}
+        onClick={() => toggleSection("TR")}
+      >
+        <div className="toggle-container">
+          <p className="summary">Typescript y RxJS</p>
+          <span className="icon-arrow">
+            {openSection === "TR" ? <FiChevronUp /> : <FiChevronDown />}
+          </span>
+        </div>
+        <div className="content">
+          {TechImages.techDescriptions.typescriptRxjs}  {/* Descripción de las tecnologías */}
+        </div>
+      </div>
+
+
+      <hr className="horizontal-line" />
+
+      {/* Sección -> React y Angular */}
+
+      <span className="title-tech">Frameworks</span>
+
+      <div className="container-img-tech">
+        <img src={TechImages.techBuild.reactSVG} alt="" />
+        <img src={TechImages.techBuild.angularSVG} alt="" />
+      </div>
+
+      <div
+        className={`custom-details ${openSection === "F" ? "open" : ""}`}
+        onClick={() => toggleSection("F")}
+      >
+        <div className="toggle-container">
+          <p className="summary">React y Angular</p>
+          <span className="icon-arrow">
+            {openSection === "F" ? <FiChevronUp /> : <FiChevronDown />}
+          </span>
+        </div>
+        <div className="content">
+          {TechImages.techDescriptions.reactAngular}  {/* Descripción de las tecnologías */}
+        </div>
+      </div>
+
+
+      <hr className="horizontal-line" />
+
+      {/* Sección -> Wordpress y WooCommerce */}
+
+      <span className="title-tech">CMS</span>
+
+      <div className="container-img-tech">
+        <img src={TechImages.techBuild.wordpressSVG} alt="" />
+        <img src={TechImages.techBuild.wooCommerceSVG} alt="" />
+      </div>
+
+      <div
+        className={`custom-details ${openSection === "WW" ? "open" : ""}`}
+        onClick={() => toggleSection("WW")}
+      >
+        <div className="toggle-container">
+          <p className="summary">Wordpress y WooCommerce</p>
+          <span className="icon-arrow">
+            {openSection === "WW" ? <FiChevronUp /> : <FiChevronDown />}
+          </span>
+        </div>
+        <div className="content">
+          {TechImages.techDescriptions.wordpressWoocommerce}  {/* Descripción de las tecnologías */}
+        </div>
+      </div>
+
+      <hr className="horizontal-line" />
+
+      <div className="endExplication-container">
+        <span>También tengo nociones con lenguajes como PHP y Java.</span>
+
+        <span>Conozco como funcionan las bases de datos relacionales y no-relacionales, aunque no tengo conocimientos profundos sobre ellas.</span>
+
+        <span>Sigo mejorando mis conocimientos con el Back-end, específicamente con el framework Node.js.</span>
+      </div>
+
+
+      
+      
+
+    </section>
+  );
+};
+
+export default Technologies;
